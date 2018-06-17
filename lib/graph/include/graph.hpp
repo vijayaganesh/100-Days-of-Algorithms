e6986ebeb03299da13cc8e59cc1e7de61008c8ed
vijayaganesh <mvijayaganesh94@gmail.com>
2018-06-17 06:16:30 -0700

    Removed Bazel Files

---
+++ lib/graph/include/graph.hpp
@@ -0,0 +1,67 @@
+/**
+ * @author VijayaGanesh Mohan
+ */
+
+#ifndef LIB_GRAPH_HPP
+#define LIB_GRAPH_HPP
+
+
+// C++ Standard Library
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+namespace vj_lib
+{
+
+// Modular graph data structure to accomodate all form of data as edges / vertex
+template<typename vertex_data,typename weight_data>
+class Graph
+{
+
+set<vertex_data> vertex_list;
+set<pair<vertex_data,vertex_data> > edge_list;
+
+map<pair<vertex_data,vertex_data> , weight_data> weight_list;
+
+map<vertex_data,bool> attribute_map;
+map<vertex_data , set<vertex_data> > adj_list;
+
+public:
+size_t num_vertices, num_edges;
+bool is_directed;
+
+Graph();
+
+~Graph();
+
+Graph(bool is_directed, int num_vertices = 0, int num_edges = 0 );
+
+set<vertex_data> vertices();
+
+set<pair<vertex_data, vertex_data> > edges();
+
+void addEdge(vertex_data from, vertex_data to,weight_data weight);
+
+bool isVisited(vertex_data vertex);
+
+void addVertex(vertex_data vertex);
+
+void markVisited(vertex_data vertex);
+
+
+};
+
+}// vj_lib namespace
+
+
+
+
+
+
+#endif
+
+
---
+++ lib/graph/test/graph_test.cpp
@@ -0,0 +1,37 @@
+/**
+ * @author VijayaGanesh Mohan
+ */
+
+#include "../include/graph.hpp"
+
+#include <iostream>
+#include <set>
+#include <map>
+
+using namespace std;
+using namespace vj_lib;
+
+
+int main()
+{
+    Graph<int,int> g(true);
+
+    g.addVertex(1);
+    g.addVertex(2);
+    g.addVertex(3);
+
+    g.addEdge(1,3,1);
+    g.addEdge(1,3,1);
+
+    set<pair<int,int> > edge_list = g.edges();
+    set<pair<int,int> >::iterator iter;
+
+    cout<<"Graph Summary"<<endl;
+    for (iter = edge_list.begin(); iter!= edge_list.end();iter++)
+    {
+        pair<int,int> k_v = *iter;
+        cout<<k_v.first<<"--------->"<<k_v.second<<endl;
+    }
+
+    return 0;
+}
